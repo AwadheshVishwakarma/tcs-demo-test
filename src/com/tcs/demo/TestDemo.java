5f06b55b5d140c2c5de9fa7040fae579b80f70c0
AwadheshVishwakarma <imavii916@gmail.com>
2022-03-21 22:39:34 -0700

    first demo commit

---
+++ src/com/tcs/demo/TestDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.tcs.demo;
+
+/**
+ * @author springuser03
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("demotest");
+
+	}
+
+}
